b63aa779fac645e344f60d824d360ae320ef68c7
LDX <htmlguy7@yahoo.com>
2015-05-08 16:57:56 -0400

    Update Main.php

--- src/RussianRoulette/Main.php
+++ src/RussianRoulette/Main.php
@@ -14,13 +14,22 @@ class Main extends PluginBase {
 
   public function onCommand(CommandSender $sender, Command $command, $label, array $args) {
     if(strtolower($command->getName()) == "bang") {
+      if(!($sender->hasPermission("russianroulette") || $sender->hasPermission("russianroulette.command") || $sender->hasPermission("russianroulette.command.bang"))) {
+      return false;
+    }
       $chambers = $this->getConfig()->get("Chambers");
+      if($chambers < 2) {
+        $sender->sendMessage("Not enough chambers.");
+        $this->getLogger()->warning("Not enough chambers.");
+        return true;
+      }
       if(mt_rand(1,$chambers) == 1) {
         $sender->kill();
         $sender->sendMessage("Unlucky...");
-        return true;
+      } else {
+        $sender->sendMessage("You got lucky...");
       }
-      $sender->sendMessage("You got lucky...");
     }
+    return true;
   }
 }
