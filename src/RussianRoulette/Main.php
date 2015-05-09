c0d3a88467c730ebb65cfd83bc7d62893f1affea
Gamecrafter <pro_victor@ymail.com>
2015-05-08 21:40:43 -0700

    Update Main.php

--- src/RussianRoulette/Main.php
+++ src/RussianRoulette/Main.php
@@ -6,30 +6,30 @@ use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\utils\Config;
 
-class Main extends PluginBase {
+class Main extends PluginBase{
 	
-  public function onEnable() {
-    $this->saveDefaultConfig();
-  }
+    public function onEnable(){
+        $this->saveDefaultConfig();
+    }
 
-  public function onCommand(CommandSender $sender, Command $command, $label, array $args) {
-    if(strtolower($command->getName()) == "bang") {
-      if(!($sender->hasPermission("russianroulette") || $sender->hasPermission("russianroulette.command") || $sender->hasPermission("russianroulette.command.bang"))) {
-      return false;
+    public function onCommand(CommandSender $sender, Command $command, $label, array $args){
+        if(strtolower($command->getName()) == "bang"){
+            if(!($sender->hasPermission("russianroulette") || $sender->hasPermission("russianroulette.command") || $sender->hasPermission("russianroulette.command.bang"))) {
+                return false;
+    	    }
+      	    $chambers = $this->getConfig()->get("chambers");
+            if($chambers < 2){
+        	$sender->sendMessage("Not enough chambers.");
+        	return true;
+      	    }
+            if(mt_rand(1, $chambers) == 1){
+            	$sender->kill();
+        	$sender->sendMessage("Unlucky...");
+      	    } 
+      	    else{
+                $sender->sendMessage("You got lucky...");
+            }
+            return true;
+        }
     }
-      $chambers = $this->getConfig()->get("Chambers");
-      if($chambers < 2) {
-        $sender->sendMessage("Not enough chambers.");
-        $this->getLogger()->warning("Not enough chambers.");
-        return true;
-      }
-      if(mt_rand(1,$chambers) == 1) {
-        $sender->kill();
-        $sender->sendMessage("Unlucky...");
-      } else {
-        $sender->sendMessage("You got lucky...");
-      }
-    }
-    return true;
-  }
 }
