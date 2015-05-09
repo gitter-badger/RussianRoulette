9a02463071f29446c2e776a62420cbd60250f348
Gamecrafter <pro_victor@ymail.com>
2015-05-09 11:13:59 -0700

    Update Main.php

--- src/RussianRoulette/Main.php
+++ src/RussianRoulette/Main.php
@@ -1,11 +1,13 @@
 <?php
 
-namespace RussianRoulette\Main;
+namespace RussianRoulette;
 
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat;
+use pocketmine\Player;
 
 class Main extends PluginBase{
 	
@@ -15,20 +17,25 @@ class Main extends PluginBase{
 
     public function onCommand(CommandSender $sender, Command $command, $label, array $args){
         if(strtolower($command->getName()) == "bang"){
-            if(!($sender->hasPermission("russianroulette") || $sender->hasPermission("russianroulette.command") || $sender->hasPermission("russianroulette.command.bang"))) {
-                return false;
-    	    }
-      	    $chambers = $this->getConfig()->get("chambers");
-            if($chambers < 2){
-        	$sender->sendMessage("Not enough chambers.");
-        	return true;
-      	    }
-            if(mt_rand(1, $chambers) == 1){
-            	$sender->kill();
-        	$sender->sendMessage("Unlucky...");
-      	    } 
-      	    else{
-                $sender->sendMessage("You got lucky...");
+            if($sender instanceof Player){
+            	if(!($sender->hasPermission("russianroulette") || $sender->hasPermission("russianroulette.command") || $sender->hasPermission("russianroulette.command.bang"))) {
+                    return false;
+    	    	}
+      	    	$chambers = $this->getConfig()->get("chambers");
+            	if($chambers < 2){
+        	    $sender->sendMessage("You don't have enough chambers.");
+        	    return true;
+      	    	}
+            	if(mt_rand(1, $chambers) == 1){
+            	    $sender->kill();
+        	    $sender->sendMessage("Unlucky...");
+      	    	} 
+      	    	else{
+                    $sender->sendMessage("You got lucky...");
+            	}
+            }	
+            else{
+            	$sender->sendMessage(TextFormat::RED."You can only play Russian Roulette in-game.");
             }
             return true;
         }
